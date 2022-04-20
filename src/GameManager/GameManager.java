7831447edf3532523d0e5e8813ef8bd7920d2210
pR0land <f.roland.c@gmail.com>
2022-04-20 13:40:36 +0200

    Update GameManager.java

--- src/GameManager/GameManager.java
+++ src/GameManager/GameManager.java
@@ -9,6 +9,7 @@ public class GameManager {
     private JButton button1;
     private JTextField TextField;
     private String name = "Jens";
+    private String lokation = "Din mor";
     ArrayList<String> firstPart = new ArrayList<String>(Arrays.asList("Der var engang en dreng som hed @%#", 
     																  "Nu skal i hører historien om @%# som var en papegøje", 
     																  "Der var engang en moden kvinde kendt som @%#",
@@ -17,13 +18,16 @@ public class GameManager {
     																  "Nu skal i hører om rotten @%#",
     																  "Nu skal i hører om nørden @%#",
     																  "Der var engang en bjørn den hed @%#"));
-    ArrayList<String> SecondPart = new ArrayList<String>(Arrays.asList("NAME elskede at spille @%#",
+    ArrayList<String> secondPart = new ArrayList<String>(Arrays.asList("NAME elskede at spille @%#",
     																   "@%# var NAME's ynglingsting og den skulle altid med på eventyr",
     																	"NAME kunne godt lide at have @%# med sig overalt det føltes betrykkende",
     																	"NAME havde jordens største @%#",
     																	"NAME tog altid sit sværd med sig fordi den ofte manglede @%#",
     																	"NAME glemte altid sin @%# når NAME skulle på tur",
     																	"NAME fik tit at vide NAME var god til at @%#"));
+    ArrayList<String> thirdPart = new ArrayList<String>(Arrays.asList("NAME måtte nu bevæge sig mod @%#",
+    																	"'Nu går jeg til @%#' sagde NAME",
+    																	"NAME fløj ud af vinduet mod @%#"));
     public static void main(String[] args){
         createAndShow();
     }
