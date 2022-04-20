deb3d803fc299d3d0e72ad5746ac92e6d94f9b33
JensHejsel <ytnomicx@gmail.com>
2022-04-20 14:16:20 +0200

    GUI

--- src/GameManager/GameManager.java
+++ src/GameManager/GameManager.java
@@ -2,30 +2,37 @@ package GameManager;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GameManager {
+public class GameManager extends JFrame implements ActionListener{
     public static void main(String[] args) { createAndShow(); }
     private static void createAndShow() {
 
         //Create and set up the window.
         JFrame frame = new JFrame("HelloWorldSwing");
-        JPanel panel = new JPanel();
-        BoxLayout layout = new BoxLayout(panel, BoxLayout.X_AXIS);
-        panel.setLayout(layout);
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new FlowLayout(FlowLayout.CENTER,10,300));
+        mainPanel.setLocation(50,50);
+        mainPanel.setPreferredSize(new Dimension(50,50));
+        mainPanel.setBackground(Color.black);
 
-        frame.setMinimumSize(new Dimension(400, 400));
+        frame.setMinimumSize(new Dimension(1000, 1000));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //Add the ubiquitous "Hello World" label.
         JButton hostButton = new JButton("Host Game");
         JButton joinButton = new JButton("Join Game");
-        panel.add(hostButton);
-        panel.add(joinButton);
-
-        //Display the window.
-        frame.add(panel);
+        hostButton.setAlignmentY(50);
+        mainPanel.add(hostButton);
+        mainPanel.add(joinButton);
+        frame.add(mainPanel);
         frame.pack();
         frame.setVisible(true);
-        //Hej jens
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
