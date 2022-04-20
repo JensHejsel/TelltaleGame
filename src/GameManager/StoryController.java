5c26079480e6e3a1adec823f57519859a26e1faf
pR0land <f.roland.c@gmail.com>
2022-04-20 15:35:07 +0200

    Update StoryController.java

--- src/GameManager/StoryController.java
+++ src/GameManager/StoryController.java
@@ -1,11 +1,16 @@
 package GameManager;
 
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Arrays;
 
 public class StoryController {
+	private Random randGene = new Random();
+	
+	private int storyState = 0; 
 	private String name = "Jens";
     private String lokation = "Din mor";
+    
     ArrayList<String> firstPart = new ArrayList<String>(Arrays.asList("Der var engang en dreng som hed @%#", 
     																  "Nu skal i hører historien om @%# som var en papegøje", 
     																  "Der var engang en moden kvinde kendt som @%#",
@@ -31,4 +36,54 @@ public class StoryController {
 																		"NAME opdagede hvor utroligt @%# LOKATION var blevet",
 																		"",
 																		""));
+    ArrayList<String> fifthPart = new ArrayList<String>(Arrays.asList(""));
+    ArrayList<String> sixthPart = new ArrayList<String>(Arrays.asList(""));
+    ArrayList<String> seventhPart = new ArrayList<String>(Arrays.asList(""));
+    ArrayList<String> eightthPart = new ArrayList<String>(Arrays.asList(""));
+    ArrayList<String> ninethPart = new ArrayList<String>(Arrays.asList(""));
+    ArrayList<String> tenthPart = new ArrayList<String>(Arrays.asList(""));
+    
+    public String getNextLine() {
+    	switch(storyState) {
+    		case 0:
+    			storyState++;
+    			return findNextLine(firstPart);
+    		case 1:
+    			storyState++;
+    			return findNextLine(secondPart);
+    		case 2:
+    			storyState++;
+    			return findNextLine(thirdPart);
+    		case 3:
+    			storyState++;
+    			return findNextLine(fourthPart);
+    		case 4:
+    			storyState++;
+    			return findNextLine(fifthPart);
+    		case 5:
+    			storyState++;
+    			return findNextLine(sixthPart);
+    		case 6:
+    			storyState++;
+    			return findNextLine(seventhPart);
+    		case 7:
+    			storyState++;
+    			return findNextLine(eightthPart);
+    		case 8:
+    			storyState++;
+    			return findNextLine(ninethPart);
+    		case 9:
+    			storyState = 0;
+    			return findNextLine(tenthPart);
+    		default:
+    			return null;
+    	}
+    }
+    private String findNextLine(ArrayList<String> inputList) {
+    	String returnString = inputList.get(randGene.nextInt(0, inputList.size()));
+    	returnString.replaceAll("@%#", "__________");
+    	returnString.replaceAll("NAME", name);
+    	returnString.replaceAll("LOKATION", lokation);
+    	return returnString;
+    }
 }
