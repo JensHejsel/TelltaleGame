4e1760746641f0849496d395df176efeec6b5ade
pR0land <f.roland.c@gmail.com>
2022-04-20 14:16:50 +0200

    diller

--- src/GameManager/GameManager.java
+++ src/GameManager/GameManager.java
@@ -8,26 +8,8 @@ import javax.swing.*;
 public class GameManager {
     private JButton button1;
     private JTextField TextField;
-    private String name = "Jens";
-    private String lokation = "Din mor";
-    ArrayList<String> firstPart = new ArrayList<String>(Arrays.asList("Der var engang en dreng som hed @%#", 
-    																  "Nu skal i hører historien om @%# som var en papegøje", 
-    																  "Der var engang en moden kvinde kendt som @%#",
-    																  "Der var engang en onkel som gik under aliaset @%#",
-    																  "Der var engang en hemmelig agent kendt som @%#", 
-    																  "Nu skal i hører om rotten @%#",
-    																  "Nu skal i hører om nørden @%#",
-    																  "Der var engang en bjørn den hed @%#"));
-    ArrayList<String> secondPart = new ArrayList<String>(Arrays.asList("NAME elskede at spille @%#",
-    																   "@%# var NAME's ynglingsting og den skulle altid med på eventyr",
-    																	"NAME kunne godt lide at have @%# med sig overalt det føltes betrykkende",
-    																	"NAME havde jordens største @%#",
-    																	"NAME tog altid sit sværd med sig fordi den ofte manglede @%#",
-    																	"NAME glemte altid sin @%# når NAME skulle på tur",
-    																	"NAME fik tit at vide NAME var god til at @%#"));
-    ArrayList<String> thirdPart = new ArrayList<String>(Arrays.asList("NAME måtte nu bevæge sig mod @%#",
-    																	"'Nu går jeg til @%#' sagde NAME",
-    																	"NAME fløj ud af vinduet mod @%#"));
+    
+    
     public static void main(String[] args){
         createAndShow();
     }
---
+++ src/GameManager/StoryController.java
@@ -0,0 +1,34 @@
+package GameManager;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class StoryController {
+	private String name = "Jens";
+    private String lokation = "Din mor";
+    ArrayList<String> firstPart = new ArrayList<String>(Arrays.asList("Der var engang en dreng som hed @%#", 
+    																  "Nu skal i hører historien om @%# som var en papegøje", 
+    																  "Der var engang en moden kvinde kendt som @%#",
+    																  "Der var engang en onkel som gik under aliaset @%#",
+    																  "Der var engang en hemmelig agent kendt som @%#", 
+    																  "Nu skal i hører om rotten @%#",
+    																  "Nu skal i hører om nørden @%#",
+    																  "Der var engang en bjørn den hed @%#"));
+    ArrayList<String> secondPart = new ArrayList<String>(Arrays.asList("NAME elskede at spille @%#",
+    																   "@%# var NAME's ynglingsting og den skulle altid med på eventyr",
+    																	"NAME kunne godt lide at have @%# med sig overalt det føltes betrykkende",
+    																	"NAME havde jordens største @%#",
+    																	"NAME tog altid sit sværd med sig fordi den ofte manglede @%#",
+    																	"NAME glemte altid sin @%# når NAME skulle på tur",
+    																	"NAME fik tit at vide NAME var god til at @%#"));
+    ArrayList<String> thirdPart = new ArrayList<String>(Arrays.asList("NAME måtte nu bevæge sig mod @%#",
+    																	"'Nu går jeg til @%#' sagde NAME",
+    																	"NAME fløj ud af vinduet mod @%#",
+    																	"NAME hopped af sted på 2 ben på vej mod @%#",
+    																	"Op af skorstenen gik det, nu var NAME på vej mod @%#"));
+    ArrayList<String> fourthPart = new ArrayList<String>(Arrays.asList("Da NAME ankom til LOKATION skete det utænkelige hans @%# eksploderede",
+																		"NAME fløj hen over LOKATION, da NAME så hvad hvordan stedet var blevet @%# blev NAME chokeret",
+																		"NAME opdagede hvor utroligt @%# LOKATION var blevet",
+																		"",
+																		""));
+}
