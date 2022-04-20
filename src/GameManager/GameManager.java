34503eb03c59219dabe58ce496c895f88be4c915
JensHejsel <ytnomicx@gmail.com>
2022-04-20 12:23:51 +0200

    neger

--- src/GameManager/GameManager.java
+++ src/GameManager/GameManager.java
@@ -1,8 +1,21 @@
 package GameManager;
 
-public class GameManager {
+import javax.swing.*;
 
-		public static void main(String[] args) {
-			System.out.println("diller");
-		}
+public class GameManager {
+    private JButton button1;
+    private JTextField TextField;
+
+    public static void main(String[] args){
+        createAndShow();
+    }
+    private static void createAndShow() {
+        JFrame mainFrame = new JFrame("Telltale Game");
+
+        mainFrame.setSize(500, 400);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setLocationRelativeTo(null);
+        mainFrame.setResizable(false);
+        mainFrame.setVisible(true);
+    }
 }
--- src/module-info.java
+++ src/module-info.java
@@ -1,2 +1,3 @@
 module telltaleGame {
+    requires java.desktop;
 }
